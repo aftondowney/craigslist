0a4749d2cda01a009d44d09f17494331d1e15f99
afton downey and matt knutson <aaike001@gmail.com>
2016-04-04 10:04:10 -0700

    add new-category component

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('category');
+    return this.store.findAll('category')
   },
 
+  actions: {
+    saveCategory(params) {
+      var newCategory = this.store.createrRecord('category', params);
+      newCategory.save();
+      this.transitionTo('index');
+    },
+  }
 });
